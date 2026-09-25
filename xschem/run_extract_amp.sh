e6e8d8f9bae4f787f724e17678e36334f90af4c4
R. Timothy Edwards <tim@opencircuitdesign.com>
2026-09-25 13:42:24 -0400

    Added a "digisub" boundary around the POR, since it is supposed to be in the 1.2V domain only.
    Signed-off-by: R. Timothy Edwards <tim@opencircuitdesign.com>

--- xschem/run_extract_amp.sh
+++ xschem/run_extract_amp.sh
@@ -3,8 +3,8 @@ mkdir -p ../netlist/schematic
 
 project=por_amp
 
-echo ${PDK_ROOT:=/usr/share/pdk} > /dev/null
-echo ${PDK:=ihp-sg13cmos5l} > /dev/null
+export PDK_ROOT=${PDK_ROOT:-/home/tim/gits}
+export PDK=${PDK:-ihp-sg13cmos5l}
 
 # xschem -n -s -r -x -q --tcl "set top_is_subckt 1" --rcfile $PDK_ROOT/$PDK/libs.tech/xschem/xschemrc -o ../netlist/schematic -N $project.spice $project.sch
 
